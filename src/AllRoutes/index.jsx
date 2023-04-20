4e5732fcecd8c5a22a9c4aa2da3cf0f70c51dd42
atul <atul.etech2011@gmail.com>
2023-04-20 19:03:48 +0530

    test

--- src/AllRoutes/index.jsx
+++ src/AllRoutes/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import {  Password, Random, VerificationPage, Website } from "../pages";
+import {  Password, VerificationPage, Website } from "../pages";
 
 const AllRoutes = () => {
   return (
@@ -9,7 +9,7 @@ const AllRoutes = () => {
       <Route path="/" element={<VerificationPage />} />
       <Route path="/password" element={<Password />} />
       <Route path="/website" element={<Website />} />
-      <Route path="/random" element={<Random />} />
+      
     </Routes>
   );
 };
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,5 +1,4 @@
 import VerificationPage from "./VerificationPage";
 import Password from "./Password";
 import Website from "./Website";
-import Random from "./Random";
-export { VerificationPage, Password, Website, Random};
+export { VerificationPage, Password, Website, };
